4a18e21d99eda2caa72f301e166e745240a4f53c
Maharavo RANDRIAMANAREFO <hei.maharavo@gmail.com>
2023-08-23 21:45:17 +0300

    Added CustomerDAO abstract Class

---
+++ Prog2/src/main/java/ExamenProg2/com/Prog2/Repository/CustomerDAO.java
@@ -0,0 +1,14 @@
+package ExamenProg2.com.Prog2.Repository;
+
+import ExamenProg2.com.Prog2.model.customer;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public abstract class CustomerDAO {
+    public abstract List<customer> getAllCustomer();
+    public abstract void insertCustomer(customer toInsert);
+    public abstract customer getIdCustomer(int id);
+    public abstract customer updateCustomer(int id, customer toUpdate);
+    public abstract List<customer> deleteCustomer(int id);
+}
