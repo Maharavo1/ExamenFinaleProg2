2f079984edca39e4f5cdf7ddf7f55e74595a1066
Maharavo RANDRIAMANAREFO <hei.maharavo@gmail.com>
2023-08-24 09:27:58 +0300

    Added the Film Class

---
+++ Prog2/src/main/java/ExamenProg2/com/Prog2/Repository/FilmDAO.java
@@ -0,0 +1,15 @@
+package ExamenProg2.com.Prog2.Repository;
+
+
+import ExamenProg2.com.Prog2.model.film;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public abstract class FilmDAO {
+    public abstract List<film> getAllFilm();
+    public abstract  void insertFilm(film toInsert);
+    public abstract film getIdFilm(int id);
+    public abstract film updateFilm(int id, film toUpdate);
+    public abstract List<film> deleteFilm(int id);
+}
