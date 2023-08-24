427eab845ed03b1e483999b5fb804ff951982aef
Maharavo RANDRIAMANAREFO <hei.maharavo@gmail.com>
2023-08-24 11:28:37 +0300

    Added the ProjectionDAO Class

---
+++ Prog2/src/main/java/ExamenProg2/com/Prog2/Repository/ProjectionDAO.java
@@ -0,0 +1,15 @@
+package ExamenProg2.com.Prog2.Repository;
+
+
+import ExamenProg2.com.Prog2.model.projection;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public abstract class ProjectionDAO {
+    public abstract List<projection> getAllProjection();
+    public abstract  void insertProjection(projection toInsert);
+    public abstract projection getIdProjection(int id);
+    public abstract projection updateProjection(int id, projection toUpdate);
+    public abstract List<projection> deleteProjection(int id);
+}
