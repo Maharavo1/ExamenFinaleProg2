c3d879bf99f69095138595a6b27d1cc7cca63823
Maharavo RANDRIAMANAREFO <hei.maharavo@gmail.com>
2023-08-24 11:30:48 +0300

    Added the RoomController Class

---
+++ Prog2/src/main/java/ExamenProg2/com/Prog2/Controller/RoomController.java
@@ -0,0 +1,47 @@
+package ExamenProg2.com.Prog2.Controller;
+
+import ExamenProg2.com.Prog2.Service.RoomService;
+import ExamenProg2.com.Prog2.model.room;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+public class RoomController {
+    private RoomService roomService;
+
+
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping("room")
+    public List<room> getAllRooms() {
+        return roomService.getAllRooms();
+    }
+
+    @PostMapping("roomInsert")
+    public void insertRoom(@RequestBody room toInsert) {
+        roomService.insertRooms(toInsert);
+    }
+
+    @GetMapping("/roomId/{id}")
+    public room getRoomById(@PathVariable int id) {
+        return roomService.getIdRooms(id);
+    }
+    @GetMapping("/roomAvalaible")
+    public List<room> getAvailableRooms(@RequestParam("dateTime") String dateTime) {
+        LocalDateTime localDateTime = LocalDateTime.parse(dateTime);
+        return roomService.getAvailableRooms(localDateTime);
+    }
+    @PutMapping("/roomUpdate/{id}")
+    public room updateRoom(@PathVariable int id, @RequestBody room toUpdate) {
+        return roomService.updateRooms(id, toUpdate);
+    }
+
+    @DeleteMapping("/roomDelete/{id}")
+    public List<room> deleteRoom(@PathVariable int id) {
+        return roomService.deleteRooms(id);
+    }
+}
