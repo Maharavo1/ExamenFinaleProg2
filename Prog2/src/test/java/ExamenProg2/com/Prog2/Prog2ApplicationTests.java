f23f3f9fc2e5514bf500b62aaf0fb5196b2dfe58
Maharavo RANDRIAMANAREFO <hei.maharavo@gmail.com>
2023-08-25 13:48:29 +0300

    Prog2ApplicationTests.jv

---
+++ Prog2/src/test/java/ExamenProg2/com/Prog2/Prog2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ExamenProg2.com.Prog2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
