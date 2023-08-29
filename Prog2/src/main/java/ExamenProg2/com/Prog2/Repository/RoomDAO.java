fea8f33d275bbe095ce90f3b97c3ff5e7d8a028c
Maharavo1 <132753016+Maharavo1@users.noreply.github.com>
2023-08-29 23:18:48 +0300

    Update RoomDAO.java

--- Prog2/src/main/java/ExamenProg2/com/Prog2/Repository/RoomDAO.java
+++ Prog2/src/main/java/ExamenProg2/com/Prog2/Repository/RoomDAO.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
-@Service
+@Repository
 public abstract class RoomDAO {
     public abstract List<room> getAllRoom();
 
