d79d86f8834a511b974450d9ea5a6fc451ceaee3
Maharavo1 <132753016+Maharavo1@users.noreply.github.com>
2023-08-29 23:14:26 +0300

    Update FilmCrud.java

--- Prog2/src/main/java/ExamenProg2/com/Prog2/Repository/FilmCrud.java
+++ Prog2/src/main/java/ExamenProg2/com/Prog2/Repository/FilmCrud.java
@@ -9,7 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-@Service
+@Repository
 public class FilmCrud extends FilmDAO {
     private Connection connection;
 
