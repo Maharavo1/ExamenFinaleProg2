a3fa4dffe269fe1538e7e9fa204ce9d2f6029560
Maharavo1 <132753016+Maharavo1@users.noreply.github.com>
2023-08-30 19:11:12 +0300

    Update ProjectionDAO.java

--- Prog2/src/main/java/ExamenProg2/com/Prog2/Repository/ProjectionDAO.java
+++ Prog2/src/main/java/ExamenProg2/com/Prog2/Repository/ProjectionDAO.java
@@ -2,7 +2,7 @@ package ExamenProg2.com.Prog2.Repository;
 
 
 import ExamenProg2.com.Prog2.model.projection;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
